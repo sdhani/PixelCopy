99feead8f810128f0ec048f580769f3d1e7f0697
Shaina Lowenthal <shaina.lowenthal96@myhunter.cuny.edu>
2019-05-31 17:05:58 -0400

    addRows

--- pixel.js
+++ pixel.js
@@ -12,6 +12,31 @@ let changeColor = () => {
 
     }
 }
-
 changeColor();
+
+let addRow = () =>
+{
+    let button = document.getElementById('addRow');
+    let table = document.getElementById("myTable");
+    let rows = Array.from(document.getElementsByTagName("TR"));
+    let p_prime = rows[0].cloneNode(true);
+    
+    
+            let newRow = document.createElement("tr");
+          
+            let newCell = document.createElement("td"); //inside the tr
+            
+        for(let i =0; i < rows[0].childElementCount; i++) //goes up until the length of the rows
+       {
+            newRow.appendChild(document.createElement("td"));
+        }
+
+            table.appendChild(newRow);     
+}
+
+let deleteRow = () =>
+{
+    
+}
+
 
