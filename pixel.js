963ca31bf80b6ee3e3221e96ffa4b979b104bb8c
shania <shania.dhani46@cuny.edu>
2019-05-31 13:45:38 -0400

    Add Column, Remove Column capabilities added, closes #5

--- pixel.js
+++ pixel.js
@@ -5,8 +5,6 @@ let changeColor = () => {
         if(cell.addEventListener('mousedown',function(){
             if(cell.addEventListener('mouseup',function(){
                 cell.style.backgroundColor = "red";
-
-            
             }));
         }));
 
@@ -15,3 +13,32 @@ let changeColor = () => {
 
 changeColor();
 
+let addColumn = () => {
+    let columns = document.getElementsByTagName('tr');
+    let addCol = document.getElementById('addColumn');
+
+    for(let row of columns){
+        addCol.addEventListener('click', function(){
+            let newCol = document.createElement("td");
+            row.appendChild(newCol);
+        });
+    }
+}
+
+addColumn();
+
+let removeColumn = () => {
+    let columns = document.getElementsByTagName('tr');
+    let removeCol = document.getElementById('subtractColumn');
+    
+
+    for(let row of columns){
+        removeCol.addEventListener('click', function(){
+            row.removeChild(row.firstChild);
+            // let newCol = document.createElement("td");
+            // row.appendChild(newCol);
+        });
+    }
+}
+
+removeColumn();
