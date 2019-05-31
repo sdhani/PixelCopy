13916d7f2648ea1e594dd117d9331754751e38e3
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2019-05-31 14:39:38 -0400

    created fill, reset, and change color functions, closes #4

--- pixel.js
+++ pixel.js
@@ -1,17 +1,61 @@
-let changeColor = () => {
-    let cells = document.getElementsByTagName("TD");
+/*
+    Lawrence Castillo
+*/
 
+/* FUNCTIONS */
+
+/* change color of cell based on input */
+let modColor = (loc,col) => {
+    loc.style.backgroundColor = col;
+}
+
+/**
+ *  change color of all cells based on input
+ * */
+let allColor = (col) => {
     for(let cell of cells){
-        if(cell.addEventListener('mousedown',function(){
-            if(cell.addEventListener('mouseup',function(){
-                cell.style.backgroundColor = "red";
-
-            
-            }));
-        }));
-
+        modColor(cell,col);
     }
 }
 
-changeColor();
+/* MAIN RUNTIME */
 
+/* gather array of all cells */
+const cells     = document.getElementsByTagName("TD");
+let   myColor   = "Yellow"; /* need to get current color from dropdown */
+let   isDrawing = false;
+
+/**
+ *  access each cell in array of cells
+ *  listen for mouse events, modify cell color property for 
+ *  mousedown and mousemove
+ * */
+for(let cell of cells){
+    
+    cell.addEventListener('mousedown',event => {
+        isDrawing = true;
+        modColor(cell,"Purple");
+    });
+
+    cell.addEventListener('mousemove',event => {
+        if(isDrawing){
+            modColor(cell,"Purple");
+        }
+    });
+
+    cell.addEventListener('mouseup',event => {
+        if(isDrawing){
+            isDrawing = false;
+        }
+    });
+}
+
+/* clear color of all cells to default  */
+if(true === false){
+    allColor("gray");
+}
+
+/* clear color of all cells to default  */
+if(true === false){
+    allColor(myColor);
+}
