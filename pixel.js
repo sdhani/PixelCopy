6f625684b9a53fd3feb49bfce6d0e98135f00b49
shania <shania.dhani46@cuny.edu>
2019-06-01 18:34:01 -0400

    edited addEventListener syntax for add/remove column/row javascript functions

--- pixel.js
+++ pixel.js
@@ -1,63 +1,7 @@
-/**
+/*
  *  Table Manipulation Script
  */
 
-
-/* FUNCTIONS */
-
-/* Color Mod Functions */
-
-/* change color of cell based on input */
-let modColor = (loc,col) => {
-    loc.style.backgroundColor = col;
-}
-
-/* change color of all cells based on input */
-let allColor = (col) => {
-    for(let cell of cells){
-        modColor(cell,col);
-    }
-}
-
-/* Table Expansion and Contraction Functions */
-
-/* add 1 column at the end of the table */
-document.getElementById('addColumn').addEventListener('click', function(){
-    let rows = document.getElementsByTagName('tr');
-    for(let row of rows){
-        let newCol = document.createElement("td");
-        row.appendChild(newCol);
-    } 
-});
-
-/* remove 1 column at the end of the table */
-document.getElementById('subtractColumn').addEventListener('click', function(){
-    let columns = document.getElementsByTagName('tr');
-    for(let row of columns){
-        row.removeChild(row.firstChild);
-    }
-});
-
-/* add 1 row at the end of the table */
-let addRow = () => {
-    let button = document.getElementById('addRow');
-    let table = document.getElementById("myTable");
-    let rows = Array.from(document.getElementsByTagName("TR"));
-    let p_prime = rows[0].cloneNode(true);
-    let newRow = document.createElement("tr");      
-    let newCell = document.createElement("td"); //inside the tr
-            
-    //goes up until the length of the rows
-    for(let i =0; i < rows[0].childElementCount; i++){ 
-        newRow.appendChild(document.createElement("td"));
-    }
-    table.appendChild(newRow);     
-}
-
-/* delete 1 row at the end of the table */
-let deleteRow = () => {
-    document.getElementById("myTable").deleteRow(0);
-}
 
 /* MAIN */
 
@@ -66,47 +10,99 @@ const cells     = document.getElementsByTagName("TD");
 let   myColor   = "Yellow"; /* need to get current color from dropdown */
 let   isDrawing = false;
 
-/**
+/*
  *  access each cell in array of cells
  *  listen for mouse events, modify cell color property for 
  *  mousedown and mousemove
- * */
-for(let cell of cells){
-    
-    cell.addEventListener('mousedown',event => {
-        isDrawing = true;
-        modColor(cell,"Purple");
-    });
+//  * */
+// for(let cell of cells){
+document.getElementsByTagName("TD").addEventListener('mousedown',event => {
+    isDrawing = true;
+    modColor(cell,"Purple");
+});
 
-    cell.addEventListener('mousemove',event => {
-        if(isDrawing){
-            modColor(cell,"Purple");
-        }
-    });
+document.getElementsByTagName("TD").addEventListener('mousemove',event => {
+  if(isDrawing){
+    modColor(cell,"Purple");
+  }
+});
 
-    cell.addEventListener('mouseup',event => {
-        if(isDrawing){
-            isDrawing = false;
-        }
-    });
+  cell.addEventListener('mouseup',event => {
+    if(isDrawing){
+      isDrawing = false;
+    }
+  });
 }
 
 /* clear color of all cells to default  */
 if(true === false){
-    allColor("gray");
+  allColor("gray");
 }
 
 /* clear color of all cells to default  */
 if(true === false){
-    allColor(myColor);
+  allColor(myColor);
 }
 
-/* add 1 column to the end of the table
+/* add 1 column to the end of the table */
 if(true === false){
     addColumn();
 }
 
-/* remove 1 column to the end of the table
+/* remove 1 column to the end of the table*/
 if(true === false){
     removeColumn();
-}*/
+}
+
+
+
+/* Table Expansion and Contraction Functions */
+
+/* add 1 column at the end of the table */
+document.getElementById('addColumn').addEventListener('click', function(){
+  let rows = document.getElementsByTagName('tr');
+  for(let row of rows){
+    let newCol = document.createElement("td");
+    row.appendChild(newCol);
+  } 
+});
+
+/* remove 1 column at the end of the table */
+document.getElementById('deleteColumn').addEventListener('click', function(){
+  let columns = document.getElementsByTagName('tr');
+  for(let row of columns){
+    row.removeChild(row.firstChild);
+  }
+});
+
+/* add 1 row at the end of the table */
+document.getElementById('addRow').addEventListener('click', function() {
+  let table = document.getElementById("myTable");
+  let rows = Array.from(document.getElementsByTagName("TR"));
+  let newRow = document.createElement("tr");      
+          
+  //goes up until the length of the rows
+  for(let i = 0; i < rows[0].childElementCount; i++){ 
+    newRow.appendChild(document.createElement("td"));
+  }
+  table.appendChild(newRow);     
+});
+
+/* delete 1 row at the end of the table */
+document.getElementById('deleteRow').addEventListener('click', function(){
+  document.getElementById("myTable").deleteRow(0);
+});
+
+/* Color Mod Functions */
+
+/* change color of cell based on input */
+let modColor = (loc,col) => {
+  loc.style.backgroundColor = col;
+}
+
+/* change color of all cells based on input */
+let allColor = (col) => {
+  for(let cell of cells){
+    modColor(cell,col);
+  }
+}
