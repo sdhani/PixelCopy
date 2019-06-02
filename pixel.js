5ddd4f699894d447d6457bb0471bc5d0b1bf2d85
shania <shania.dhani46@cuny.edu>
2019-06-02 08:37:50 -0400

    merge error fixed with updates to add/remove cell and inclusion of up=to-date color fcns

--- pixel.js
+++ pixel.js
@@ -1,9 +1,6 @@
 /*
  *  Table Manipulation Script
  */
-
-<<<<<<< HEAD
-=======
 /* FUNCTIONS */
 
 /* Drawing Functions */
@@ -14,113 +11,79 @@
  *  mousedown and mousemove
  * */
 let beginDraw = () => {
-    for(let cell of cells){
+  for(let cell of cells){
 
-        cell.addEventListener('mousedown',event => {
-            isDrawing = true;
-            console.log(cell);
-            console.log(cells.length);
-            modColor(cell,"#ffcf40");
-        });
+      cell.addEventListener('mousedown',event => {
+          isDrawing = true;
+          console.log(cell);
+          console.log(cells.length);
+          modColor(cell,"#ffcf40");
+      });
 
-        cell.addEventListener('mousemove',event => {
-            if(isDrawing){
-                modColor(cell,"#ffcf40");
-            }
-        });
+      cell.addEventListener('mousemove',event => {
+          if(isDrawing){
+              modColor(cell,"#ffcf40");
+          }
+      });
 
-        cell.addEventListener('mouseup',event => {
-            if(isDrawing){
-                isDrawing = false;
-            }
-        });
-    }
+      cell.addEventListener('mouseup',event => {
+          if(isDrawing){
+              isDrawing = false;
+          }
+      });
+  }
 } 
 
 /* Color Mod Functions */
 
 /* change color of cell based on input */
 let modColor = (loc,clr) => {
-    loc.style.backgroundColor = clr;
+  loc.style.backgroundColor = clr;
 }
 
 /* helper to grab current color value */
 let allColorHelper = () => {
-    //get current color
-    allColor("#adcfe6");
+  //get current color
+  allColor("#adcfe6");
 }
 
 /* change color of all cells based on input */
 let allColor = (clr) => {
-    for(let cell of cells){
-        modColor(cell,clr);
-    }
+  for(let cell of cells){
+      modColor(cell,clr);
+  }
 }
 
 /* helper to grab current color value */
 let remColorHelper = () => {
-    //get current color
-    remColor("#FFC0CB");
+  //get current color
+  remColor("#FFC0CB");
 }
 
 /* change color of all cells based on input */
 let remColor = (clr,def) => {
-    for(let cell of cells){
-        if(cell.style.backgroundColor == "gainsboro"){
-            console.log("true");
-            modColor(cell,clr);
-        }
-    }
+  for(let cell of cells){
+      if(cell.style.backgroundColor == "gainsboro"){
+          console.log("true");
+          modColor(cell,clr);
+      }
+  }
 }
 
-/* Table Expansion and Contraction Functions */
 
-/* add 1 column at the end of the table */
-let addColumn = () => {
-    //document.getElementById('addColumn').addEventListener('click', function(){
-    let rows = document.getElementsByTagName('tr');
-    for(let row of rows){
-        let newCol = document.createElement("td");
-        row.appendChild(newCol);
-        modColor(newCol,"gainsboro");
-    }
-    //});
+
+
+
+ /* clear color of all cells to default  */
+if(false === initColor){
+  allColor("gainsboro");
+  initColor = true;
 }
 
-/* remove 1 column at the front of the table */
-let deleteColumn = () => {
-    //document.getElementById('deleteColumn').addEventListener('click', function(){
-    let columns = document.getElementsByTagName('tr');
-    for(let row of columns){
-        row.removeChild(row.firstChild);
-    }
-    //});
-}
-
-/* add 1 row at front of the table */
-let addRow = () => {
-    //let button = document.getElementById('addRow');
-    let table = document.getElementById("myTable");
-    let rows = Array.from(document.getElementsByTagName("TR"));
-    //let p_prime = rows[0].cloneNode(true);
-    let newRow = document.createElement("tr");      
-    //let newCell = document.createElement("td"); //inside the tr
-            
-    //goes up until the length of the rows
-    for(let i =0; i < rows[0].childElementCount; i++){ 
-        newRow.appendChild(document.createElement("td"));
-        modColor(newRow,"gainsboro");
-    }
-    table.appendChild(newRow);
-    //console.log(cells);
-}
-
-/* delete 1 row at the front of the table */
-let deleteRow = () => {
-    document.getElementById("myTable").deleteRow(0);
-}
->>>>>>> 1b351b7f2c7f86bbc72aa7bd39e8ca78e5353b3c
-
+/* listen for mouse event */
+document.onmousemove = function(){
+  beginDraw();
+};
 /* MAIN */
 
 let initCells = false;
@@ -133,7 +96,6 @@ let i = 0;
 const cells = document.getElementsByTagName("TD");
 console.log(cells);
 
-<<<<<<< HEAD
 /*
  *  access each cell in array of cells
  *  listen for mouse events, modify cell color property for 
@@ -230,15 +192,3 @@ let allColor = (col) => {
     modColor(cell,col);
   }
 }
-=======
-/* clear color of all cells to default  */
-if(false === initColor){
-    allColor("gainsboro");
-    initColor = true;
-}
-
-/* listen for mouse event */
-document.onmousemove = function(){
-    beginDraw();
-};
->>>>>>> 1b351b7f2c7f86bbc72aa7bd39e8ca78e5353b3c
